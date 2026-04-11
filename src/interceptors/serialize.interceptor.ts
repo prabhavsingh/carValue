7977e95b98f5dc82cd9eff1deb69b093ba79b2b8
Prabhav Singh <65345194+prabhavsingh@users.noreply.github.com>
2026-04-11 23:26:24 +0530

    custom data serlization,interceptors

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,37 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+  UseInterceptors,
+} from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { map, Observable } from 'rxjs';
+// import { UserDto } from 'src/users/dtos/user.dto';
+
+interface ClassConstructor {
+  new (...args: any[]): {};
+}
+
+export function Serialize(dto: ClassConstructor) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
+  intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
+    // run something before a request is handled by the request handlder
+    console.log('i am running before the handler', context);
+
+    return handler.handle().pipe(
+      map((data: any) => {
+        //run something before the response is sent out
+        console.log('i am running before response is sent out', data);
+
+        return plainToClass(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import {
   Body,
+  ClassSerializerInterceptor,
   Controller,
   Delete,
   Get,
@@ -8,12 +9,19 @@ import {
   Patch,
   Post,
   Query,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import {
+  Serialize,
+  SerializeInterceptor,
+} from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private userService: UsersService) {}
 
@@ -22,8 +30,11 @@ export class UsersController {
     this.userService.create(body.email, body.password);
   }
 
+  //   @UseInterceptors(new SerializeInterceptor(UserDto))
+  //   @Serialize(UserDto)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
+    console.log('handler is running');
     const user = await this.userService.findOne(parseInt(id));
     if (!user) {
       throw new NotFoundException('user not found');
