c1bad12fc90af671c12683e9790cd2adac58ed2b
Prabhav Singh <65345194+prabhavsingh@users.noreply.github.com>
2026-04-14 03:59:46 +0530

    Reports ->relations,association

---
+++ src/reports/dtos/report.dto.ts
@@ -0,0 +1,24 @@
+import { Expose, Transform } from 'class-transformer';
+
+export class ReportDto {
+  @Expose()
+  id!: number;
+  @Expose()
+  price!: number;
+  @Expose()
+  make!: string;
+  @Expose()
+  model!: string;
+  @Expose()
+  year!: number;
+  @Expose()
+  lng!: number;
+  @Expose()
+  lat!: number;
+  @Expose()
+  milage!: number;
+
+  @Transform(({ obj }) => obj.user.id)
+  @Expose()
+  userId!: number;
+}
